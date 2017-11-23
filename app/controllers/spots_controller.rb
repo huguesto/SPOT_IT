3f1e1919916140f70af27c32292e7a67601f0de4
hugues tonnet <hugues.tonnet@gmail.com>
2017-11-23 15:35:21 +0100

    add atachniary form

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -8,11 +8,14 @@ class SpotsController < ApplicationController
   end
 
   def create
+    @spot = Spot.new(spot_params)
+    @spot.place = Place.find(params[:place_id])
+    @spot.save
   end
 
   private
 
   def spot_params
-  require(:spot).permit(:comment, :photo)
+  params.require(:spot).permit(:comment, :photo)
   end
 end
